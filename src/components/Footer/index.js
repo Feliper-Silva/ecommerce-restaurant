92ae6b8c65f4f971f3e02b9be7d2aa9a5a2df555
Felipe Silva <felipe726silva@gmail.com>
2022-01-14 20:00:08 -0300

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa';
+
+import {
+  FooterContainer,
+  FooterWrap,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcons,
+  SocialIconLink
+} from './FooterElements';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">Pizza</SocialLogo>
+            <SocialIcons>
+              <SocialIconLink
+                href="/"
+                target="_blanck"
+                aria-label="Facebook"
+                rel="noopener noreferrer"
+              >
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink
+                href="/"
+                target="_blanck"
+                aria-label="Instagram"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink
+                href="/"
+                target="_blanck"
+                aria-label="Youtube"
+                rel="noopener noreferrer"
+              >
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink
+                href="/"
+                target="_blanck"
+                aria-label="Linkedin"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
