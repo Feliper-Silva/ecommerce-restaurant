fc284bcee43993f80e95d799880e66fd71752487
Felipe Silva <84206933+Feliper-Silva@users.noreply.github.com>
2022-01-15 00:40:19 -0300

    Update GlobalStyles.js

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   *{
-    box-sizing: border-boz;
+    box-sizing: border-box;
     margin: 0;
     padding: 0;
     font-family: 'Kanit', sans-serif;
